17ef7e34b985ec94e89339fab2741b02b54e9729
Darin Armstrong <darin.marmstrong@gmail.com>
2019-06-19 10:51:26 -0600

    created view controllers and linked views to appropriate controllers

---
+++ PlaylistCoreData2/Controllers/View Controllers/SongListTableViewController.swift
@@ -0,0 +1,46 @@
+//
+//  SongListTableViewController.swift
+//  PlaylistCoreData2
+//
+//  Created by Darin Marcus Armstrong on 6/19/19.
+//  Copyright © 2019 Darin Marcus Armstrong. All rights reserved.
+//
+
+import UIKit
+
+class SongListTableViewController: UITableViewController {
+    
+    @IBOutlet weak var songNameTextField: UITextField!
+    @IBOutlet weak var artistNameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
+    }
+    
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+}
